710c0173ad7616718b93a2d6fd3177d585f12d1c
Franyelica <franyelicavalentina.fvgf@gmail.com>
2026-03-26 23:51:08 -0500

    Actualizacion Program.cs
    - Se remueve el if de la configuracion del HTTP request pipeline

--- LogisticaEnvios/LogisticaEnvios/Program.cs
+++ LogisticaEnvios/LogisticaEnvios/Program.cs
@@ -46,14 +46,12 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseCors("FrontendPolicy");
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
